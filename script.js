7be235f7bddfb14c2fd0ebfb798c418a446bd983
EricUnternahr <133649199+EricUnternahr@users.noreply.github.com>
2023-10-05 11:17:28 +0200

    modif du 5/10

--- script.js
+++ script.js
@@ -72,11 +72,11 @@ const bodyElement = document.body;
 
 
 if (switchElement.checked) {
-    bodyElement.classList.remove('body-light');
-    bodyElement.classList.add('body-dark');
-} else {
     bodyElement.classList.remove('body-dark');
     bodyElement.classList.add('body-light');
+} else {
+    bodyElement.classList.remove('body-light');
+    bodyElement.classList.add('body-dark');
 }
 });
 
